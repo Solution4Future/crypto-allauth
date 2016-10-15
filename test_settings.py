c3c257e6bace67f3e79c4c9f2a80a3f9888ceed0
Kamil Gałuszka <galuszkak@gmail.com>
2016-10-15 05:57:58 +0200

    added travis support with tox.ini tests

---
+++ test_settings.py
@@ -0,0 +1,13 @@
+SECRET_KEY = 'psst'
+SITE_ID = 1
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
