a420723f7dd09fa8d9fcd4c00a2f78cca149f987
Kamil Gałuszka <galuszkak@gmail.com>
2016-10-15 06:13:53 +0200

    update to settings and crypto_allauth

--- test_settings.py
+++ test_settings.py
@@ -11,3 +11,20 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    'crypto_allauth.providers.trezor',
+
+]
